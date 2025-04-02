f48c6637c283bb06ee6bb50dd013c1819887ab72
Daggerhashimoto <59483938+6ios@users.noreply.github.com>
2025-04-02 12:14:58 -0500

    Working!!!!

--- ShallowSeekServer/src/main/java/com/example/shallowseekserver/model/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shallowseekserver.model
-
-data class Message(
-    val id: Long? = null,
-    val content: String,
-    val sender: String,
-    val timestamp: Long = System.currentTimeMillis()
-)
