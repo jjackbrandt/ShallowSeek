7cd2b8368f11fa785aeb7c84655bacdb47ac55c8
jjackbrandt <59483938+6ios@users.noreply.github.com>
2025-05-07 19:46:59 -0500

    Fix Compose plugin configuration for compatibility with Kotlin 1.9.10

--- ShallowSeek/app/build.gradle.kts
+++ ShallowSeek/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.compose") version "1.5.3"
 }
 
 android {
--- ShallowSeek/build.gradle.kts
+++ ShallowSeek/build.gradle.kts
@@ -2,5 +2,12 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
 }
